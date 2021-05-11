f4b31c24a883c9d9e378f6c19d0fa2169711c556
P-Hsgw <contact@p-hsgw.com>
2021-05-11 13:27:52 +0900

    Update README.md

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,21 +8,7 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+  <p className="text-red-600 text-center">HELLO, I am Gatsby Starter with pre-configured Tailwind CSS</p>
   </Layout>
 )
 
